4a53a4a60b766cdd5cd571495405f90d1f62ff29
Dima <xdryx@ngs.ru>
2017-05-30 12:05:20 +0000

    OS fix

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require('express');
-const os = require("os");
+const ua = require("ua-parser");
 const app = express();
-
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res){
     res.redirect('/api/whoami');
-})
+});
 
 app.get('/api/whoami', function (req, res) {
     const ip = req.headers['x-forwarded-for'];
     const language = req.acceptsLanguages()[0];
-    const operatingSystem = os.type() + '; ' + os.platform() + '; ' + os.arch();
+    var operatingSystem = ua.parse(req.headers['user-agent']).os.toString();
     const resultObj = {};
     resultObj.ipaddress = ip;
     resultObj.language = language;
     resultObj.software = operatingSystem;
-    
-    console.log(operatingSystem);
-  res.send(resultObj)
-})
+    res.send(resultObj);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Header parser app is running on port', app.get('port'));
